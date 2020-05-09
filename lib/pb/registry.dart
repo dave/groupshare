886e7e00668d26a941766cf4763f5b6bf07fc509
Dave Brophy <dave@brophy.uk>
2020-05-09 15:24:50 +0100

    move proto to root

--- lib/pb/registry.dart
+++ lib/pb/registry.dart
@@ -1,6 +1,6 @@
 import 'package:protobuf/protobuf.dart';
-import 'package:groupshare/pb/data/share.pb.dart' as data_share;
 import 'package:groupshare/pb/data/foo/foo.pb.dart' as data_foo_foo;
+import 'package:groupshare/pb/data/share.pb.dart' as data_share;
 
 final types = TypeRegistry([
   data_foo_foo.Foo(),
