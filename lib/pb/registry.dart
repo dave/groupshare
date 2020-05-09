e3af8625a71f7be6e480e29ea015801f61550531
Dave Brophy <dave@brophy.uk>
2020-05-09 14:50:07 +0100

    fixes

--- lib/pb/registry.dart
+++ lib/pb/registry.dart
@@ -3,7 +3,7 @@ import 'package:groupshare/pb/data/foo/foo.pb.dart' as data_foo_foo;
 import 'package:groupshare/pb/data/share.pb.dart' as data_share;
 
 final types = TypeRegistry([
-  data_foo_foo.Foo(),
   data_share.Share(),
+  data_foo_foo.Foo(),
 ]);
 
