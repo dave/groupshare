e6032f98a6f8f80fdc5f1e238270c7f24f8940a7
Dave Brophy <dave@brophy.uk>
2020-07-04 09:30:09 +0100

    add bar data

--- lib/pb/registry.dart
+++ lib/pb/registry.dart
@@ -1,9 +1,11 @@
 import 'package:protobuf/protobuf.dart' as protobuf;
-import 'package:groupshare/pb/groupshare/data/foo/foo.pb.dart' as groupshare_data_foo_foo;
+import 'package:groupshare/pb/groupshare/data/bar.pb.dart' as groupshare_data_data;
 import 'package:groupshare/pb/groupshare/data/share.pb.dart' as groupshare_data_data;
+import 'package:groupshare/pb/groupshare/data/foo/foo.pb.dart' as groupshare_data_foo_foo;
 
 final types = protobuf.TypeRegistry([
-  groupshare_data_foo_foo.Foo(),
+  groupshare_data_data.Bar(),
   groupshare_data_data.Share(),
+  groupshare_data_foo_foo.Foo(),
 ]);
 
